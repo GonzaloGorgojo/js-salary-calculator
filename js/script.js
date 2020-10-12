df6b623d3c2e1dd1fc32153ec86fba10c059c185
Gonzalo Gorgojo <gongorgojo@gmail.com>
2020-10-12 01:32:32 -0300

    modificaciones dom

--- js/script.js
+++ js/script.js
@@ -1,23 +1,63 @@
-var consulta_mensual = parseInt(prompt("Ingrese su sueldo Bruto Mensual","Ingrese numero sin puntos ni comas..."));
-var sueldoBruto_mensual = document.getElementsByClassName("sueldoBruto_mensual");
-sueldoBruto_mensual[0].innerHTML = consulta_mensual;
-
-var calculoBruto_anual = (consulta_mensual * 12);
-
-var sueldoBruto_anual = document.getElementsByClassName("sueldoBruto_anual");
-sueldoBruto_anual[0].innerHTML = calculoBruto_anual;
-
-
 
 function elementoCreado(){
-    var contenedor = document.createElement("p");
-    var texto = document.createTextNode("te Dije que no funciona!");
-    contenedor.appendChild(texto);
+    var contenedor = document.createElement("INPUT");
+    contenedor.style.margin = '10px';
+    contenedor.style.fontSize = '30px';
+    contenedor.setAttribute("id", "inputSueldo");
 
     var agregar = document.getElementById("nuevoElemento");
     document.body.insertBefore(contenedor, agregar);
 
+    document.getElementById("inputSueldo").addEventListener("keypress", calculoSueldo);
 }
+
+function elementoCreado_texto(){
+    var texto = document.createElement("p");
+    var parrafo = document.createTextNode("Ingrese su sueldo bruto y presione la tecla Enter al Finalizar!");
+    texto.style.fontSize = '25px';
+    texto.style.marginLeft = '10px';
+    texto.style.fontWeight = 'bold';
+    texto.appendChild(parrafo);
+
+    var algo = document.getElementById("otroElemento");
+    document.body.insertBefore(texto, algo);
+}
+
+function llamarAmbas() {
+    elementoCreado();
+    elementoCreado_texto();
+}
+
+/*
+
+*/
+
+function sueldoBruto(){
+    var consulta_mensual = document.getElementById("inputSueldo").value;
+    var sueldoBruto_mensual = document.getElementsByClassName("sueldoBruto_mensual");
+    sueldoBruto_mensual[0].innerHTML = consulta_mensual;
+
+    var calculoBruto_anual = (consulta_mensual * 12);
+
+    var sueldoBruto_anual = document.getElementsByClassName("sueldoBruto_anual");
+    sueldoBruto_anual[0].innerHTML = calculoBruto_anual;
+
+}
+
+
+
+function calculoSueldo(evento){
+
+    if (evento.which == 13) {
+        sueldoBruto();
+    }
+
+}
+
+
+
+
+
 
 
 
